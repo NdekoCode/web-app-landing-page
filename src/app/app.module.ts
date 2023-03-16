6f8ceaa525fed3e98ab8d9026e96feca3143d300
NdekoCode <arickbulakali@gmail.com>
2023-03-16 18:12:57 +0200

    chore: [project structure] add sass structure directories  and config media queries variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NotificationBarComponent } from './header/notification-bar/notification-bar.component';
+import { TopbarComponent } from './header/topbar/topbar.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NotificationBarComponent, TopbarComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/header/notification-bar/notification-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-bar',
+  templateUrl: './notification-bar.component.html',
+  styleUrls: ['./notification-bar.component.scss'],
+})
+export class NotificationBarComponent {}
