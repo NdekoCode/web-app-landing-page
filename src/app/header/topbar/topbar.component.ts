be5ef9e3515a7bb4b30791c93e5c77a916fbed44
NdekoCode <arickbulakali@gmail.com>
2023-03-16 21:57:08 +0200

    feat: [togglenav end] add toggle humberger button

--- src/app/header/topbar/topbar.component.ts
+++ src/app/header/topbar/topbar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+  styleUrls: ['./topbar.component.scss'],
 })
 export class TopbarComponent {
-
+  isClicked: boolean = false;
+  onChangeNavButton() {
+    this.isClicked = !this.isClicked;
+  }
 }
