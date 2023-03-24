be0b19848a0a7ef9348d7e33ce31cbbecb9e5da8
NdekoCode <arickbulakali@gmail.com>
2023-03-24 17:56:08 +0200

    feat: [bottom-label end] add bottom-label component and refactor navigation button style

---
+++ src/app/components/bottom-label/bottom-label.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BottomLabelComponent } from './bottom-label.component';
+
+describe('BottomLabelComponent', () => {
+  let component: BottomLabelComponent;
+  let fixture: ComponentFixture<BottomLabelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BottomLabelComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BottomLabelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
