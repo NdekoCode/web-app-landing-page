d611155309d1981333f279d63715bf9b10738555
NdekoCode <arickbulakali@gmail.com>
2023-04-09 17:04:32 +0200

    feat: add language form block

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { navLinks } from 'src/app/utils/constants';
 import { RouterLink } from 'src/app/utils/types';
 
@@ -10,10 +11,18 @@ import { RouterLink } from 'src/app/utils/types';
 export class TopbarComponent {
   isClicked: boolean = false;
   topbarLinks: RouterLink[] = navLinks;
+  defaultLanguage: string = 'fr';
+  isSelected: boolean = false;
 
   onChangeNavButton() {
     if (window.innerWidth < 992) {
       this.isClicked = !this.isClicked;
     }
+  }
+  onSubmit(arg: NgForm) {}
+  changeLanguage(e: Event, arg?: NgForm) {
+    this.isSelected = !this.isSelected;
+    arg?.onSubmit(e);
+    console.log(this.defaultLanguage);
   }
 }
