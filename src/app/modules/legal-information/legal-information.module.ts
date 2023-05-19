6ebb5e6d7d3ec8b6fbb9ab8c51c280055bdb0e9b
NdekoCode <arickbulakali@gmail.com>
2023-05-19 12:34:31 +0200

    feat: add legal information

--- src/app/modules/legal-information/legal-information.module.ts
+++ src/app/modules/legal-information/legal-information.module.ts
@@ -2,19 +2,32 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ShareModule } from './../share-module/share-module.module';
-import { RgpdTemplateComponent } from './components/rgpd-template/rgpd-template.component';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { RgpdComponent } from './rgpd/rgpd.component';
-import { CookiePolicyComponent } from './cookie-policy/cookie-policy.component';
-import { DownloadButtonComponent } from './components/download-button/download-button.component';
-import { SidebarLinksComponent } from './components/sidebar-links/sidebar-links.component';
-import { LegalDateComponent } from './components/legal-date/legal-date.component';
-import { LegalInformationComponent } from './legal-information/legal-information.component';
-import { SubcontractorsComponent } from './subcontractors/subcontractors.component';
 import { CguComponent } from './cgu/cgu.component';
+import { DownloadButtonComponent } from './components/download-button/download-button.component';
+import { LegalDateComponent } from './components/legal-date/legal-date.component';
+import { SidebarLinksComponent } from './components/sidebar-links/sidebar-links.component';
+import { CookiePolicyComponent } from './cookie-policy/cookie-policy.component';
+import { LegalInformationComponent } from './legal-information/legal-information.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { RgpdTemplateComponent } from './rgpd-template/rgpd-template.component';
+import { RgpdComponent } from './rgpd/rgpd.component';
+import { SubcontractorsComponent } from './subcontractors/subcontractors.component';
+import { TermsComponent } from './terms/terms.component';
 
 @NgModule({
-  declarations: [PrivacyPolicyComponent, RgpdTemplateComponent, RgpdComponent, CookiePolicyComponent, DownloadButtonComponent, SidebarLinksComponent, LegalDateComponent, LegalInformationComponent, SubcontractorsComponent, CguComponent],
+  declarations: [
+    PrivacyPolicyComponent,
+    RgpdTemplateComponent,
+    RgpdComponent,
+    CookiePolicyComponent,
+    DownloadButtonComponent,
+    SidebarLinksComponent,
+    LegalDateComponent,
+    LegalInformationComponent,
+    SubcontractorsComponent,
+    CguComponent,
+    TermsComponent,
+  ],
   imports: [CommonModule, ShareModule, RouterModule],
 })
 export class LegalInformationModule {}
